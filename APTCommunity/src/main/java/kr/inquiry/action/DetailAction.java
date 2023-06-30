db5dddefad719dbd1f2f6a2b61f3cb845da2250c
user <user@DESKTOP-ABLDB89>
2023-06-30 16:15:19 +0900

    김희범 1:1문의상세 작성자와 관리자만 열람가능하게 변경

--- APTCommunity/src/main/java/kr/inquiry/action/DetailAction.java
+++ APTCommunity/src/main/java/kr/inquiry/action/DetailAction.java
@@ -2,7 +2,7 @@ package kr.inquiry.action;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import javax.servlet.http.HttpSession;
 
 import kr.controller.Action;
 import kr.inquiry.dao.InquiryDAO;
@@ -13,10 +13,27 @@ public class DetailAction implements Action{
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		//글번호
+				HttpSession session = request.getSession();
+				Integer user_num = (Integer)session.getAttribute("user_num");
+				Integer user_auth = (Integer)session.getAttribute("user_auth");
+				
+				if (user_num==null) {
+					request.setAttribute("notice_msg", "로그인이 필요한 서비스입니다");
+					request.setAttribute("notice_url", request.getContextPath()+"/member/loginForm.do");
+					return "/WEB-INF/views/common/alert_singleView.jsp";
+				}
+		
+				//글번호
 				int in_num = Integer.parseInt(request.getParameter("in_num"));
 				InquiryDAO dao = InquiryDAO.getInstance();
 				InquiryVO inquiry = dao.getInquiry(in_num);
+				
+				//작성자가 아니거나, 관리자가 아닐경우에 리스트목록으로
+				if (user_num != inquiry.getMem_num() && user_auth != 9) {
+					request.setAttribute("notice_msg", "조회 권한이 없습니다.");
+					request.setAttribute("notice_url", request.getContextPath()+"/inquiry/list.do");
+					return "/WEB-INF/views/common/alert_singleView.jsp";
+				}
 				
 				//HTML 태그를 허용하지 않음
 				inquiry.setTitle(StringUtil.useNoHtml(inquiry.getTitle()));
