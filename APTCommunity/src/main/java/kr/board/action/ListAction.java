87012352626348f551bf4b9cb6d43ae6bb90036b
myYeom <myYeom@BOOK-191PIMOOTS>
2023-07-01 18:20:50 +0900

    염유진 리스트수정

--- APTCommunity/src/main/java/kr/board/action/ListAction.java
+++ APTCommunity/src/main/java/kr/board/action/ListAction.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import kr.board.dao.BoardDAO;
 import kr.board.vo.BoardVO;
@@ -17,6 +18,14 @@ public class ListAction implements Action{
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		//로그인 되어있는지 체크, 아닐시 로그인폼으로 이동
+		HttpSession session = request.getSession();
+		Integer user_num = (Integer)session.getAttribute("user_num");
+		if (user_num==null) {
+			request.setAttribute("notice_msg", "로그인이 필요한 서비스입니다");
+			request.setAttribute("notice_url", request.getContextPath()+"/member/loginForm.do");
+			return "/WEB-INF/views/common/alert_singleView.jsp";
+		}
 		String pageNum = request.getParameter("pageNum");
 		if(pageNum == null) pageNum = "1";
 		
